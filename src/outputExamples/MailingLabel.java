335190515a89ae67da12b628689ba003b89993a3
Justin <19leej@elmbrookstudents.org>
2018-09-13 13:56:55 -0500

    Mailing Label

---
+++ src/outputExamples/MailingLabel.java
@@ -0,0 +1,10 @@
+package outputExamples;
+
+public class MailingLabel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Justin Lee\nW222N2287 Glenwood Lane\nWaukesha, WI 53186");
+	}
+
+}
