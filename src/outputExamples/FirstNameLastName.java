d241fbb73baa3ebf36d93fd99a895f3989d75634
Justin <19leej@elmbrookstudents.org>
2018-09-13 13:11:45 -0500

    First name Last name

---
+++ src/outputExamples/FirstNameLastName.java
@@ -0,0 +1,11 @@
+package outputExamples;
+
+public class FirstNameLastName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Justin");
+		System.out.println("Lee");
+	}
+
+}
