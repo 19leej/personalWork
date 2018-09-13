3d9937856af4927e53b449483d91663be6501678
Justin <19leej@elmbrookstudents.org>
2018-09-13 13:52:43 -0500

    5 Likes and Dislikes

---
+++ src/outputExamples/LikesAndDislikes.java
@@ -0,0 +1,15 @@
+package outputExamples;
+
+public class LikesAndDislikes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Likes \t\t\t Dislikes");
+		System.out.println("1. Computers \t\t 1. Weird seafood");
+		System.out.println("2. Food \t\t 2. Meanies");
+		System.out.println("3. Videogames \t\t 3. Homework");
+		System.out.println("4. Movies \t\t 4. Carrying bass drum 6");
+		System.out.println("5. Sports \t\t 5. Waking up ");
+	}
+
+}
