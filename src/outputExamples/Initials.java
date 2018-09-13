d0359a5353bc71cf706713211fe739de360dccf9
Justin <19leej@elmbrookstudents.org>
2018-09-13 14:05:02 -0500

    Initials

---
+++ src/outputExamples/Initials.java
@@ -0,0 +1,21 @@
+package outputExamples;
+
+public class Initials {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("       J  L       ");
+		System.out.println("       J  L       ");
+		System.out.println("       J  L       ");
+		System.out.println("       J  L       ");
+		System.out.println("       J  L       ");
+		System.out.println("       J  L       ");
+		System.out.println("       J  L       ");
+		System.out.println("J      J  L       ");
+		System.out.println("J      J  L       ");
+		System.out.println("J      J  L       ");
+		System.out.println(" JJJJJJ   LLLLLLLL");
+		
+	}
+
+}
