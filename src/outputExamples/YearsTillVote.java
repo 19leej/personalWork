5d66e78885c89744dc061c1b1812d13cb3fa1800
Justin <19leej@elmbrookstudents.org>
2018-09-13 13:25:54 -0500

    Years Till I Can Vote

---
+++ src/outputExamples/YearsTillVote.java
@@ -0,0 +1,10 @@
+package outputExamples;
+
+public class YearsTillVote {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1 more year till I can vote!!");
+	}
+
+}
