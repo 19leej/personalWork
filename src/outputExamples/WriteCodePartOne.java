71f253f818500f17402690603a0a346c1713b145
Justin <19leej@elmbrookstudents.org>
2018-09-18 15:01:05 -0500

    1 Write code part 1.doc
    Activity

---
+++ src/outputExamples/WriteCodePartOne.java
@@ -0,0 +1,13 @@
+package outputExamples;
+
+public class WriteCodePartOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int MAXSTUDENS = 30;
+		final char MIDDLEINTIAL = 'P';
+		int numStudents = 0;
+		System.out.println(MAXSTUDENS);
+	}
+
+}
