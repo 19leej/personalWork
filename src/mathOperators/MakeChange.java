2620163d229be6aec746305af31fb30255e64576
Unknown <19leej@elmbrookstudents.org>
2018-09-25 17:22:11 -0500

    Final Make Change

--- src/mathOperators/MakeChange.java
+++ src/mathOperators/MakeChange.java
@@ -9,22 +9,40 @@ public class MakeChange {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
-		double change = -999.0;
+		double initialChange = -999.0;
+		int newChange = -999;
+		int twenties = -999;
+		int tens = -999;
+		int fives = -999;
 		int ones = -999;
-		double quarters = -999;
+		int quarters = -999;
 		int dimes = -999;
 		int nickels = -999;
 		int pennies = -999;
 		System.out.println("What is the amount of change?");
-		change = input.nextDouble();
-		ones = (int)change;
-		change = change - ones;
-		quarters = change / 0.25;
-		change = change - ((int)quarters * 0.25);
-		change = BigDecimal.change;
-		System.out.println("Give the customer " + ones + " one's.");
-		System.out.println((int)quarters);
-		System.out.println(change);
+		initialChange = input.nextDouble();
+		System.out.println("The total amount of change is $" + initialChange + ".");
+		initialChange = initialChange * 100.000000000001;
+		newChange = (int) initialChange;
+		twenties = newChange / 2000;
+		newChange = newChange - (twenties * 2000);
+		tens = newChange / 1000;
+		newChange = newChange - (tens * 1000);
+		fives = newChange / 500;
+		newChange = newChange - (fives * 500);
+		ones = newChange / 100;
+		newChange = newChange - (ones * 100);
+		quarters = newChange / 25;
+		newChange = newChange - (quarters * 25);
+		dimes = newChange / 10;
+		newChange = newChange - (dimes * 10);
+		nickels = newChange / 5;
+		newChange = newChange - (nickels * 5);
+		pennies = newChange / 1;
+		newChange = newChange - (pennies);
+		System.out.println("Give the customer " + twenties + " twenties, " + tens + " tens, " + fives + " fives, "
+				+ ones + " ones, " + quarters + " quarters, " + dimes + " dimes, " + nickels + " nickels, and "
+				+ pennies + " pennies.");
 		input.close();
 	}
 
