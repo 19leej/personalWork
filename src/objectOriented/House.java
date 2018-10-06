53075a092f1be7c0f56ab966f469646e1414cb9b
Unknown <19leej@elmbrookstudents.org>
2018-10-06 17:22:01 -0500

    Fill the Sandbox Programming Opportunity

--- src/objectOriented/House.java
+++ src/objectOriented/House.java
@@ -22,4 +22,29 @@ public class House {
 		System.out.println("It will take " + time + " minutes to paint the wall.");
 
 	}
+
+	public void Sandbox1() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("What is the length of the sandbox in feet?");
+		double length = input.nextDouble();
+		System.out.println("What is the width of the sandbox?");
+		double width = input.nextDouble();
+		System.out.println("What is the depth of the sandbox?");
+		double depth = input.nextDouble();
+		System.out.println("How much san(in cubic feet) can you transport per hour?");
+		double transport = input.nextDouble();
+		double volume = length * width * depth;
+		double time = volume / transport;
+		System.out.println("It will take " + time + " hours to fill the sandbox.");
+	}
+	public void Sandbox2(double length, double width, double depth, double transport) {
+		double volume = length * width * depth;
+		double time = volume / transport;
+		System.out.println("It will take " + time + " hours to fill the sandbox.");
+	}
+	public double Sandbox3(double length, double width, double depth, double transport) {
+		double volume = length * width * depth;
+		double time = volume / transport;
+		return time;
+	}
 }
