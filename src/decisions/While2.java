8e2f13aa9f38181fe6da2b8c99b1041612d3a141
Justin <19leej@elmbrookstudents.org>
2018-10-23 15:07:27 -0500

    While Loop Practice

---
+++ src/decisions/While2.java
@@ -0,0 +1,23 @@
+package decisions;
+
+import java.util.Scanner;
+
+public class While2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		int number = 0;
+		int choice = 0;
+		System.out.println("Please enter a number.(9999 to quit)");
+		choice = input.nextInt();
+		while (choice != 9999) {
+			number = choice * 2;
+			System.out.println("Your number times two is: " + number);
+			System.out.println("Please enter a number.(9999 to quit)");
+			choice = input.nextInt();
+		}
+		input.close();
+	}
+
+}
