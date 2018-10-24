5d7105e63222a3a6c9ca7e37e801f06380208eda
Justin <19leej@elmbrookstudents.org>
2018-10-24 15:08:17 -0500

    DoWhile Loop Practice

---
+++ src/decisions/DoWhile1.java
@@ -0,0 +1,20 @@
+package decisions;
+
+import java.util.Scanner;
+
+public class DoWhile1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter an integer.");
+		int number = input.nextInt();
+		do {
+			System.out.println("Magic product: " + number * 17);
+			System.out.println("Enter an integer.(9999 to quit)");
+			number = input.nextInt();
+		} while (number != 9999);
+		input.close();
+	}
+
+}
