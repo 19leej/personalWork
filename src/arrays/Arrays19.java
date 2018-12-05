28a598466773314c334eebcc99c59a9dcebdaa9f
Justin <19leej@elmbrookstudents.org>
2018-12-05 14:52:04 -0600

    Changes and Stuff

--- src/arrays/Arrays19.java
+++ src/arrays/Arrays19.java
@@ -11,10 +11,6 @@ public class Arrays19 {
 		names.add(new Friend("Tom", 17));
 		names.add(new Friend("Mary", 24));
 		names.add(new Friend("Null", 0));
-		names.add(3, names.get(2));
-		names.add(2, names.get(0));
-		names.add(0, names.get(3));
-		System.out.println(names);
 	}
 
 }
