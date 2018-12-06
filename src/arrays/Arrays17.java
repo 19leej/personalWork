c95f0abd12fad33374d99b4b93ac8fe74f8d25f7
Justin <19leej@elmbrookstudents.org>
2018-12-06 14:12:25 -0600

    More ArrayList Stuff

--- src/arrays/Arrays17.java
+++ src/arrays/Arrays17.java
@@ -9,15 +9,14 @@ public class Arrays17 {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
 		ArrayList<String> names = new ArrayList<String>();
-		int counter = 0;
-		for (counter = 0; counter < 3; counter++) {
-			System.out.println("What is the name?");
-			names.add(input.nextLine());
-		}
 		names.add("Bob");
 		names.add(names.size() - 1, "Jane");
-		names.add(0, "Billy");
-		names.add(7, "Joe");
+		names.add(1, "Billy");
+		System.out.println("What is the name?");
+		names.add(input.nextLine());
+		for (int one = 0; one < names.size(); one++) {
+			System.out.println(names.get(one));
+		}
 		input.close();
 	}
 
